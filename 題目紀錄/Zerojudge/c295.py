74a2e053ba710cf6ad61629a95dcffae9be987c0
stdcyanout <stdcyanout@gmail.com>
2024-02-23 18:05:54 +0800

    20240223

---
+++ 題目紀錄/Zerojudge/c295.py
@@ -0,0 +1,12 @@
+import sys
+def getline():
+    return sys.stdin.readline().strip()
+def getlines():
+    return 
+
+def main():
+    s=getlines()
+    n,m=s[0].strip()
+
+if __name__=="__main__":
+    main()
--- 題目紀錄/隨堂練習/第2週/p1.py
+++ 題目紀錄/隨堂練習/第2週/p1.py
@@ -6,18 +6,22 @@ def getlines():
     return sys.stdin.readlines()
 
 def main():
-    a,b,c=map(int,getline().split())
-    if b**2-4*a*c<0:
-        print("No real root")
-        return
-    x1=(-b+math.sqrt(b**2-4*a*c))//(2*a)
-    x2=(-b-math.sqrt(b**2-4*a*c))//(2*a)
-    if x1>x2:
-        print("Two different roots x1=%d , x2=%d"%(x1,x2))
-    elif x1==x2:
-        print("Two same roots x=%d"%(x1))
-    else:
-        print("Two different roots x1=%d , x2=%d"%(x2,x1))
+    while True:
+        try:
+            a,b,c=map(int,getline().split())
+            if b**2-4*a*c<0:
+                print("No real root")
+                continue
+            x1=(-b+math.sqrt(b**2-4*a*c))//(2*a)
+            x2=(-b-math.sqrt(b**2-4*a*c))//(2*a)
+            if x1>x2:
+                print("Two different roots x1=%d , x2=%d"%(x1,x2))
+            elif x1==x2:
+                print("Two same roots x=%d"%(x1))
+            else:
+                print("Two different roots x1=%d , x2=%d"%(x2,x1))
+        except:
+            return
 
 if __name__=="__main__":
     main()
